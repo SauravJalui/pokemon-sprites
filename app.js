582f8af232a93cab4352a1fe6baa08a58aaf9986
SauravJalui <saurav.jalui-codes@outlook.com>
2021-07-22 11:38:05 +0530

    added pokemon sprites

---
+++ app.js
@@ -0,0 +1,20 @@
+// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+
+const container = document.querySelector("#container");
+const baseURL =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+
+for (let i = 1; i <= 898; i++) {
+  // instead of hardcoding the length, find a way to get the last element from the website
+  const pokemon = document.createElement("div");
+  pokemon.classList.add("pokemon");
+  const label = document.createElement("span");
+  label.innerText = `#${i}`;
+  const newImg = document.createElement("img");
+  newImg.src = `${baseURL}${i}.png`;
+  container.append(newImg);
+
+  pokemon.appendChild(newImg);
+  pokemon.appendChild(label);
+  container.appendChild(pokemon);
+}
